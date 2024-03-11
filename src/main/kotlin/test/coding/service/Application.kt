157d854330998e82d131a61fcd8c5cf5cc02d96d
Mr Swordsteel <swordsteel@gmail.com>
2024-03-11 22:15:11 +0100

    disable R2DBC in Application

--- src/main/kotlin/test/coding/service/Application.kt
+++ src/main/kotlin/test/coding/service/Application.kt
@@ -1,9 +1,13 @@
 package test.coding.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+/*
+ * disable R2DBC in application by excluding the autoconfiguration
+ */
+@SpringBootApplication(exclude = [R2dbcAutoConfiguration::class])
 class Application
 
 fun main(vararg args: String) {
